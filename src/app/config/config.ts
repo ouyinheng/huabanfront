fc7f8dd5d3ce4f70c5342e81dcc7b7e970180e12
ouyinheng <2675379992@qq.com>
2018-10-23 23:39:57 +0800

    update

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,7 @@
-const hostPath='http://192.168.31.45:3000';
-const imgHost = '//img.hb.aicdn.com/';
+// const hostPath='http://192.168.31.45:3000';
+const hostPath='http://192.168.31.57:3000';
+
+const imgHost = 'http://img.hb.aicdn.com/';
 
 export {
   hostPath, imgHost
