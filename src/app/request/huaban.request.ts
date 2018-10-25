9f2ef14e72353e366beb7c870c2e67fbef917fbd
ouyinheng <2675379992@qq.com>
2018-10-25 17:28:02 +0800

    'search'

--- src/app/request/huaban.request.ts
+++ src/app/request/huaban.request.ts
@@ -9,4 +9,6 @@ export class HuabanService {
   constructor(private http: HttpClient) {}
   getHuabanHome = (params:any) => this.http.get(`${hostPath}/huabanhome?page=${params}`)
   getAnthorInfo = (params:any) => this.http.post(`${hostPath}/author`, params)
+  searchAuto = (params:any) => this.http.get(`${hostPath}/searchAuto?q=${params.q}&size=${params.size}`)
+  search = (params:any) => this.http.get(`${hostPath}/search?q=${params.q}`)
 }
--- src/app/router/huaban/home/home.component.ts
+++ src/app/router/huaban/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { HuabanService } from '../../../request/huaban.request'
 import {Router,ActivatedRoute} from '@angular/router';
 import {imgHost} from '../../../config/config';
@@ -11,7 +12,8 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private http:HuabanService,
-    private router:Router
+    private router:Router,
+    private titleService: Title
   ) { }
   imgList:any[] = []
   imgHost:string = '';
@@ -19,6 +21,13 @@ export class HomeComponent implements OnInit {
   tpCls:string = null;
   btnCls:string = null;
   login:string = null
+  menu:any = null;
+  banner:any = null;
+  host:string = 'http://hbfile.b0.upaiyun.com/img/home/banner/'
+  bannerImg:string = '';
+  visibleMenu:boolean = false;
+  favorite:any = null;
+  searchValue:string = ''
   ngOnInit() {
     window.addEventListener('scroll',(e:any)=>{
       let top = document.documentElement.scrollTop;
@@ -28,8 +37,20 @@ export class HomeComponent implements OnInit {
     })
     this.imgHost = imgHost;
     this.http.getHuabanHome(1).subscribe((res:any) => {
-      if(res.result)this.imgList = JSON.parse(res.result);
-      console.log('this.imgList:----->',this.imgList)
+      if(res.result) {
+        let that = this;
+        let result = res.result;
+        this.imgList = JSON.parse(result.recom);
+        this.titleService.setTitle(result.title);
+        this.menu = JSON.parse(result.menu);
+        this.menu.categories = this.menu.categories.slice(0,9)
+        this.favorite = JSON.parse(result.menu).categories;
+        console.log(this.menu)
+        this.banner = JSON.parse(result.banner)
+        let i:number = (Math.random()*5)
+        i = parseInt(i.toString());
+        this.bannerImg = this.host+this.banner[i].cover.key
+      }
     }, (err:any) => {
       console.log('err:----->',err)
     })
@@ -39,6 +60,24 @@ export class HomeComponent implements OnInit {
       if(res.result)this.imgList.push(...JSON.parse(res.result).recommends)
     }, (err:any) => {
       console.log('err:----->',err)
+    })
+  }
+  showMenu() {
+    this.visibleMenu = true;
+  }
+  search() {
+    this.http.searchAuto({q:this.searchValue,size:6}).subscribe((res:any) => {
+      console.log('值',res)
+    }, (err:any) => {
+      console.log('错误',err)
+    })
+  }
+  enter(val:string) {
+    console.log(val)
+    this.http.search({q:val}).subscribe((res:any) => {
+      console.log('值',res)
+    }, (err:any) => {
+      console.log('错误',err)
     })
   }
   navTo(type:string, urlname:string, board_id:string) {
